e1dc14b728539ff1d1b9a9f7253b519d1e7dcf21
Shayla Richard <shaylarichard@hotmail.com>
2017-12-28 15:40:56 -0700

    first add

---
+++ yellPhrase.js
@@ -0,0 +1,15 @@
+
+let yellPhrase = (phrase)=> {
+if( ! phrase || typeof phrase !== 'string') {
+  return "Please input a phrase"
+}
+  return phrase.toUpperCase()
+  }
+
+
+
+
+
+module.exports = {
+  yellPhrase
+}
